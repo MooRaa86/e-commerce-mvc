22b6c3c07ad32b2358ac4b1a5301d93bad38c7a9
7azem512 <hazemsaed5122@gmail.com>
2026-05-11 17:02:09 +0300

    home page and some caching

--- src/main/java/com/omar/ecommercemvc/config/dbConnection.java
+++ src/main/java/com/omar/ecommercemvc/config/dbConnection.java
@@ -5,30 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/ecommerce-mvc";
-
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce-mvc";
     private static final String USERNAME = "root";
-
     private static final String PASSWORD = "12345";
-
     public static Connection getConnection() {
-
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            return DriverManager.getConnection(
-                    URL,
-                    USERNAME,
-                    PASSWORD
-            );
-
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
-
             throw new RuntimeException(e);
-
         }
-
     }
 }
--- src/main/java/com/omar/ecommercemvc/controller/HomeServlet.java
+++ src/main/java/com/omar/ecommercemvc/controller/HomeServlet.java
@@ -1,5 +1,7 @@
 package com.omar.ecommercemvc.controller;
 
+import com.omar.ecommercemvc.model.Product;
+import com.omar.ecommercemvc.service.ProductService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,14 +9,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
+
+    private final ProductService productService =
+            new ProductService();
 
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
             throws ServletException, IOException {
+
+        List<Product> products =
+                productService.getAllProducts();
+
+        request.setAttribute("products", products);
 
         request.getRequestDispatcher(
                 "/views/home.jsp"
